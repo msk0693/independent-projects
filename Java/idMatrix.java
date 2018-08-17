4fcd4a3d593c39ce6f490d7e7e8c5dafef15f557
Mike Kazzi <msk0693@yahoo.com>
2018-08-17 14:55:55 -0400

    Update idMatrix.java

--- Java/idMatrix.java
+++ Java/idMatrix.java
@@ -10,30 +10,40 @@ public class idMatrix
 {
  public static void main(String args [])
  {
+  String cur = "-Current Matrix-\n";
+  String fin = "-Final Matrix-\n";
   int n = 0 ;
   Scanner in = new Scanner(System.in) ;
   System.out.print("Enter dimensions of n x n array: ") ;
   n = in.nextInt() ;
   int [] [] x = new int[n][n] ;
+  clearScreen();
   System.out.print("Fill array with 1 or 0\n") ;
   for (int r = 0 ; r < x.length ; r++)
   {
    for(int c = 0 ; c < x[0].length ; c++)
    {
 
+    display(cur, x);
+    System.out.format("Position (%d ,%d): ", r+1, c+1);
+
     x[r][c] = in.nextInt() ;
- 
+    clearScreen();
+
     while (x[r][c] != 1 && x[r][c] != 0)
     {
-     System.out.println("invalid option choose 1 or 0") ;
+     System.out.println("INVALID OPTION: Choose 1 or 0") ;
+     x[r][c] = 0;
+     display(cur, x);
+     System.out.format("Position (%d ,%d): ", r, c);
+
      x[r][c] = in.nextInt() ;
+    clearScreen();
     }
+
    }
   }
-
-  System.out.println("") ;
-
-  display(x) ;
+  display(fin, x) ;
   System.out.println("Identity Matrix : " + check_id(x)) ;
 
 
@@ -41,7 +51,7 @@ public class idMatrix
  }
  public static boolean check_id(int [] [] a)
  {
-   int count = 0 ;
+  int count = 0 ;
   for (int r = 0 ; r < a.length ; r++ )
   {
     
@@ -64,8 +74,10 @@ public class idMatrix
   }
   return true ;
  }
- public static void display(int [] [] b )
+ public static void display(String title, int [] [] b )
  {
+  System.out.print(title);
+
   for (int r = 0; r < b.length; r++) 
   {
      for (int c = 0; c < b[0].length; c++) 
@@ -79,5 +91,10 @@ public class idMatrix
   
 
 
+ }
+ public static void clearScreen()
+ {
+    System.out.print("\033[H\033[2J");  
+    System.out.flush(); 
  }
 }
